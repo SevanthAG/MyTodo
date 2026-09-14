c2c466a619ee193c0493e427d06576b1b64dba64
SevanthAG <sevanthrao1406@gmail.com>
2026-09-14 14:49:28 +0530

    Login Endpoint Created

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express, { type Express, type Request, type Response } from 'express';
 import { Pool } from "pg";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app: Express = express();
 const port = 3000;
@@ -33,6 +34,42 @@ app.post('/signup', async (req, res) => {
     user: newUser.rows[0]
   });
 });
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  const userExists = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+
+  if (userExists.rows.length === 0) {
+    res.status(400).json({
+      message: 'User does not exist'
+    });
+    return;
+  }
+
+  const user = userExists.rows[0];
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    res.status(400).json({
+      message: 'Invalid password'
+    });
+    return;
+  }
+
+  const token = jwt.sign({
+    id: user.id
+  },
+    process.env.JWT_SECRET!
+  );
+
+  res.status(200).json({
+    message: 'User logged in successfully',
+    token
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
